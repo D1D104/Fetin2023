9536e7477e63416b96623c6a1b735bdc0f7570a6
Diego Vilela <109529702+D1D104@users.noreply.github.com>
2023-08-29 11:39:24 -0300

    Arduino code
    Add Arduino code to read a RFID Tag and send to NodeMCU using serial

---
+++ megaok.ino
@@ -0,0 +1,39 @@
+#include <SPI.h>
+#include <RFID.h>
+#include <SoftwareSerial.h>
+#define SDA_DIO 9
+#define RESET_DIO 8
+RFID RC522(SDA_DIO, RESET_DIO); 
+
+SoftwareSerial espSerial(5, 6);
+
+String tag = "";
+void setup(){ 
+  
+  Serial.begin(115200);
+  espSerial.begin(115200);
+  SPI.begin(); 
+  RC522.init();
+}
+
+String aux="";
+
+void loop()
+{
+  if (RC522.isCard())
+  {
+    RC522.readCardSerial();
+    for(int i=0;i<5;i++)
+    {
+   tag+= String(RC522.serNum[i],DEC);
+    }
+    if(tag != aux){
+    //Serial.println(tag);
+    espSerial.print(tag);
+    aux= tag;
+    }
+  }
+ tag= "";
+ delay(1000);
+ aux="";
+}
